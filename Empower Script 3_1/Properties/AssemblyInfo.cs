fc634935e2d3cc68ddcb704f31103e2ae5d7fbc3
Simon Aannestad Sutterud <simon.sutterud@telenor.com>
2023-03-28 14:25:52 +0200

    Add project files.

---
+++ Empower Script 3_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower Script 3_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower Script 3_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2D073DEB-8A04-4DFA-9EF8-C213C498C3F6")]
+[assembly: AssemblyVersion("1.0.0.0")]
