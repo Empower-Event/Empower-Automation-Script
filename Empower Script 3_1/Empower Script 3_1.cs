2733eaf7b020757da04da450beea833bbef2ec5e
Simon Aannestad Sutterud <simon.sutterud@telenor.com>
2023-03-28 14:39:42 +0200

    Add hello world

--- Empower Script 3_1/Empower Script 3_1.cs
+++ Empower Script 3_1/Empower Script 3_1.cs
@@ -68,7 +68,7 @@ namespace Empower_Script_3_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-	
+			engine.GenerateInformation("Hello world!");
 		}
 	}
 }
